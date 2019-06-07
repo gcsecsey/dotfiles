194cec67605022b209d26c3067cbcb5febb64f9f
Gergely Csécsey <gcsecsey@gmail.com>
2019-06-07 14:04:03 +0200

    Update oh-my-zsh-setup.sh

--- oh-my-zsh-setup.sh
+++ oh-my-zsh-setup.sh
@@ -9,24 +9,24 @@ bash -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/m
 ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
 
 # Download zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
 
 # Download zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
 
 # Download powerlevel10k
-git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+git clone https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM}/themes/powerlevel10k
 
 # Download and set pure power config
 ( cd && curl -fsSLO https://raw.githubusercontent.com/romkatv/dotfiles-public/master/.purepower )
 echo 'source ~/.purepower' >>! ~/.zshrc
 
 # Chmod plugins to avoid ZSH COMPFIX errors
-chmod 755 $ZSH_CUSTOM/plugins/*
+chmod 755 ${ZSH_CUSTOM}/plugins/*
 
 # Change plugins in .zshrc
-sed -i -e 's/plugins=\".*\"/plugins=(docker git npm zsh-autosuggestions zsh-syntax-highlighting)/g' $HOME/.zshrc
+sed -i -e 's/plugins=\".*\"/plugins=(docker git npm zsh-autosuggestions zsh-syntax-highlighting)/g' ${HOME}/.zshrc
 
 # Change ZSH_THEME in .zshrc
-sed -i -e 's/ZSH_THEME=\".*\"/ZSH_THEME=\"powerlevel10k/powerlevel10k\"/g' $HOME/.zshrc
+sed -i -e 's/ZSH_THEME=\".*\"/ZSH_THEME=\"powerlevel10k/powerlevel10k\"/g' ${HOME}/.zshrc
 
