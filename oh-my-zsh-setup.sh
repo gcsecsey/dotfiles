a954435b8c517e8d905c4e4586ecf238ef2fac71
Gergely Csécsey <gcsecsey@gmail.com>
2019-06-07 13:39:56 +0200

    Update oh-my-zsh-setup.sh

--- oh-my-zsh-setup.sh
+++ oh-my-zsh-setup.sh
@@ -4,7 +4,7 @@
 # bash -c "$(curl -fsSL https://raw.githubusercontent.com/gcsecsey/dotfiles/master/oh-my-zsh-setup.sh)"
 
 # Install oh-my-zsh
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" --unattended
 
 # Download zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
