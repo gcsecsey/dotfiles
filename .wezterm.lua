7a6df0129be30bea191e66eaa088f3e244f20151
Gergely Csécsey <gcsecsey@gmail.com>
2023-04-18 10:54:00 +0100

    add wezterm

---
+++ .wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require 'wezterm'
+
+return {
+  font = wezterm.font('JetBrains Mono'),
+  font_size = 14.0,
+  color_scheme = 'GitHub Dark',
+  window_close_confirmation = "NeverPrompt",
+}
